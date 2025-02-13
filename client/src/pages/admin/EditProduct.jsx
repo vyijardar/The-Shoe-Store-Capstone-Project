e1f753c2f89c1190db88701749ee3c7094d34a01
feature-A <vyijardar@gmail.com>
2025-02-12 21:51:04 -0600

    Final commit

--- client/src/pages/admin/EditProduct.jsx
+++ client/src/pages/admin/EditProduct.jsx
@@ -19,115 +19,121 @@ export default function EditProduct({ product, onClose, onSave }) {
   };
 
   return (
-    <div className="modal">
-      <form onSubmit={handleSubmit}>
-        <h3>Edit Product</h3>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={editedProduct.name}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Price:
-          <input
-            type="number"
-            name="price"
-            value={editedProduct.price}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Description:
-          <textarea
-            name="description"
-            value={editedProduct.description}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Brand:
-          <input
-            type="text"
-            name="brand"
-            value={editedProduct.brand}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Category:
-          <input
-            type="text"
-            name="category"
-            value={editedProduct.category}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          Gender:
-          <select name="gender" value={editedProduct.gender} onChange={handleChange} required>
-            <option value="">Select</option>
-            <option value="men">men</option>
-            <option value="women">women</option>
-            <option value="unisex">unisex</option>
-          </select>
-        </label>
-        <label>
-          Size:
-          <input
-            type="text"
-            name="size"
-            value={editedProduct.size}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Color:
-          <input
-            type="text"
-            name="color"
-            value={editedProduct.color}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Stock:
-          <input
-            type="number"
-            name="stock"
-            value={editedProduct.stock}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Image URLs:
-          {editedProduct.image_urls.map((url, index) => (
-            <input
-              key={`image-url-${index}`}
-              type="text"
-              value={url}
-              onChange={(e) => {
-                const newImageUrls = [...editedProduct.image_urls];
-                newImageUrls[index] = e.target.value;
-                setEditedProduct({ ...editedProduct, image_urls: newImageUrls });
-              }}
-            />
-          ))}
-          <button type="button" onClick={() => setEditedProduct({ ...editedProduct, image_urls: [...editedProduct.image_urls, ""] })}>
-            Add Another Image
-          </button>
-        </label>
-        <button type="submit">Save Changes</button>
-        <button type="button" onClick={onClose}>
-          Cancel
-        </button>
-      </form>
+    <div className="modal show d-block" tabIndex="-1" role="dialog">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <form onSubmit={handleSubmit}>
+              <h3>Edit Product</h3>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  name="name"
+                  value={editedProduct.name}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <label>
+                Price:
+                <input
+                  type="number"
+                  name="price"
+                  value={editedProduct.price}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <label>
+                Description:
+                <textarea
+                  name="description"
+                  value={editedProduct.description}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Brand:
+                <input
+                  type="text"
+                  name="brand"
+                  value={editedProduct.brand}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Category:
+                <input
+                  type="text"
+                  name="category"
+                  value={editedProduct.category}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <label>
+                Gender:
+                <select name="gender" value={editedProduct.gender} onChange={handleChange} required>
+                  <option value="">Select</option>
+                  <option value="men">men</option>
+                  <option value="women">women</option>
+                  <option value="unisex">unisex</option>
+                </select>
+              </label>
+              <label>
+                Size:
+                <input
+                  type="text"
+                  name="size"
+                  value={editedProduct.size}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Color:
+                <input
+                  type="text"
+                  name="color"
+                  value={editedProduct.color}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Stock:
+                <input
+                  type="number"
+                  name="stock"
+                  value={editedProduct.stock}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Image URLs:
+                {editedProduct.image_urls.map((url, index) => (
+                  <input
+                    key={`image-url-${index}`}
+                    type="text"
+                    value={url}
+                    onChange={(e) => {
+                      const newImageUrls = [...editedProduct.image_urls];
+                      newImageUrls[index] = e.target.value;
+                      setEditedProduct({ ...editedProduct, image_urls: newImageUrls });
+                    }}
+                  />
+                ))}
+                <button type="button" onClick={() => setEditedProduct({ ...editedProduct, image_urls: [...editedProduct.image_urls, ""] })}>
+                  Add Another Image
+                </button>
+              </label>
+              <button type="submit">Save Changes</button>
+              <button type="button" onClick={onClose}>
+                Cancel
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
