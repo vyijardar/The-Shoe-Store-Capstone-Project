97d599b2790b005fad245dab9f4da95a657b8e13
feature-A <vyijardar@gmail.com>
2025-02-13 21:24:56 -0600

    Commit

--- server/db/index.js
+++ server/db/index.js
@@ -33,7 +33,7 @@ app.use(require('morgan')('dev'))
 const path = require('path');
 app.use(express.static(path.join(__dirname,'../..', 'client', 'dist')));
 // app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/dist/index.html')));
-app.get("*", (req, res) => {  res.sendFile(path.join(__dirname, '../../client/dist/index.html')) });
+//app.get("*", (req, res) => {  res.sendFile(path.join(__dirname, '../../client/dist/index.html')) });
 app.use('/assets', express.static(path.join(__dirname, '../../client/dist/assets')));
 
 //Admin Dashboard
